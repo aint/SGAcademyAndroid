cf020167a81f6ace02cdab81430b9db2547df79d
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2016-08-16 17:00:16 +0300

    Implement retrofit callback for text translation

--- YandexTranslator/app/src/main/java/com/github/aint/yandextranslator/activity/MainActivity.java
+++ YandexTranslator/app/src/main/java/com/github/aint/yandextranslator/activity/MainActivity.java
@@ -3,32 +3,86 @@ package com.github.aint.yandextranslator.activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.github.aint.yandextranslator.R;
+import com.github.aint.yandextranslator.model.TranslateJsonResponse;
+import com.github.aint.yandextranslator.service.YandexTranslateService;
+import com.google.gson.Gson;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Map;
+import java.util.TreeMap;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+public class MainActivity extends AppCompatActivity implements Callback<TranslateJsonResponse> {
 
     private static final String TAG = MainActivity.class.getName();
+
+    private static final String YANDEX_TRANSLATE_URL = "https://translate.yandex.net/api/v1.5/tr.json/";
+    private static final String API_KEY = "trnsl.1.1.20160815T165258Z.23bee0520431f4c8.5466f3d425604ca53525b97b38a76fff39e468a0";
+
+    private static final String SOMETHING_WENT_WRONG_TOAST = "Something went wrong. Try again";
+
+    private static final String KEY_REQUEST_FIELD = "key";
+    private static final String TEXT_REQUEST_FIELD = "text";
+    private static final String LANG_REQUEST_FIELD = "lang";
+
+    private Retrofit retrofit = new Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create(new Gson()))
+            .baseUrl(YANDEX_TRANSLATE_URL)
+            .build();
+
+    private YandexTranslateService translateService = retrofit.create(YandexTranslateService.class);
+
+    @BindView(R.id.textFrom) EditText textFromEditText;
+    @BindView(R.id.textTo) EditText textToEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ButterKnife.bind(this);
     }
 
     private void translateText() {
-        String textFrom = ((EditText) findViewById(R.id.textFrom)).getText().toString();
         hideKeyboard();
-        String textTo = "";//new Translator().execute(textFrom).get();
-        Log.e(TAG, textFrom +  " => " + textTo);
-        ((EditText) findViewById(R.id.textTo)).setHint(textTo);
+        translateService.translate(getRequestFieldMap()).enqueue(this);
+    }
+
+    private Map<String, String> getRequestFieldMap() {
+        Map<String, String> map = new TreeMap<>();
+        map.put(KEY_REQUEST_FIELD, API_KEY);
+        map.put(TEXT_REQUEST_FIELD, textFromEditText.getText().toString());
+        map.put(LANG_REQUEST_FIELD, "en-uk");
+        return map;
+    }
+
+    @Override
+    public void onResponse(Call<TranslateJsonResponse> call, Response<TranslateJsonResponse> response) {
+        if (response.code() == 200) {
+            String textTo = response.body().getText()[0];
+            textToEditText.setHint(textTo);
+            return;
+        }
+        Toast.makeText(this, "Oops... " + response.code(), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onFailure(Call<TranslateJsonResponse> call, Throwable t) {
+        Toast.makeText(this, SOMETHING_WENT_WRONG_TOAST, Toast.LENGTH_LONG).show();
     }
 
     private void hideKeyboard() {
@@ -55,5 +109,4 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
 }
