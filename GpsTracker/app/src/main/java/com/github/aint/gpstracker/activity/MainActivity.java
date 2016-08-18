37df54a05f16d3acab7fa102701dd29a3bf84bb9
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2016-08-18 19:26:41 +0300

    Check location permission. Try to get last location. Some refactoring

--- GpsTracker/app/src/main/java/com/github/aint/gpstracker/activity/MainActivity.java
+++ GpsTracker/app/src/main/java/com/github/aint/gpstracker/activity/MainActivity.java
@@ -4,80 +4,165 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityCompat.OnRequestPermissionsResultCallback;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.github.aint.gpstracker.R;
-import com.github.aint.gpstracker.service.GpsService;
+import com.github.aint.gpstracker.service.LocationService;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, OnRequestPermissionsResultCallback {
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+import static com.github.aint.gpstracker.service.LocationService.LAT_ATTRIBUTE;
+import static com.github.aint.gpstracker.service.LocationService.LNG_ATTRIBUTE;
+
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, OnMapClickListener,
+        OnRequestPermissionsResultCallback, ConnectionCallbacks {
 
     public static final String NEW_LOCATION_ACTION = "new_location";
 
     private final static int REQUEST_LOCATION = 121;
+    private static final String PERMISSION_NOT_GRANTED_TOAST = "Permission to location not granted";
+    private static final String YOU_ARE_HERE_LABEL = "You are here";
+
+    private GoogleApiClient googleApiClient;
 
     private GoogleMap mMap;
     private Marker currentLocationMarker;
     private Marker onClickMarker;
 
+    private Location lastLocation;
+
     private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             if (NEW_LOCATION_ACTION.equals(intent.getAction())) {
-                LatLng latLng = new LatLng(intent.getDoubleExtra("lat", 0), intent.getDoubleExtra("lng", 0));
+                LatLng latLng = new LatLng(intent.getDoubleExtra(LAT_ATTRIBUTE, 0), intent.getDoubleExtra(LNG_ATTRIBUTE, 0));
                 currentLocationMarker.setPosition(latLng);
             }
         }
     };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SupportMapFragment mapFragment =  (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
 
-        startService(new Intent(this, GpsService.class));
+        getGoogleApiClient();
 
-        registerReceiver(broadcastReceiver, new IntentFilter(NEW_LOCATION_ACTION));
+        initServiceAndReceiver();
+    }
+
+    @Override
+    protected void onStart() {
+        googleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
     }
 
     @Override
     protected void onDestroy() {
-        super.onDestroy();
         unregisterReceiver(broadcastReceiver);
+        super.onDestroy();
+    }
+
+    private void getGoogleApiClient() {
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    private void initServiceAndReceiver() {
+        startService(new Intent(this, LocationService.class));
+        registerReceiver(broadcastReceiver, new IntentFilter(NEW_LOCATION_ACTION));
+    }
+
+    private void checkLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) != PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION}, REQUEST_LOCATION);
+        }
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        setUpMap(googleMap);
+        setCurrentPosition();
+    }
+
+    private void setUpMap(GoogleMap googleMap) {
         mMap = googleMap;
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng point) {
-                if (onClickMarker == null) {
-                    onClickMarker = mMap.addMarker(new MarkerOptions().position(point));
-                }
-                onClickMarker.setPosition(point);
-            }
-        });
+        checkLocationPermission();
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMapClickListener(this);
         mMap.getUiSettings().setZoomControlsEnabled(true);
+    }
 
-        LatLng rivne = new LatLng(50.61, 26.24);
-        currentLocationMarker = mMap.addMarker(new MarkerOptions().position(rivne).title("Marker in Rivne"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(rivne, 10));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(rivne));
+    private void setCurrentPosition() {
+        LatLng location = getCurrentLocation();
+        currentLocationMarker = mMap.addMarker(new MarkerOptions().position(location).title(YOU_ARE_HERE_LABEL));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+    }
 
+    private LatLng getCurrentLocation() {
+        if (lastLocation != null) {
+            return new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+        }
+        return new LatLng(50.62, 26.25);
+    }
 
+    @Override
+    public void onMapClick(LatLng point) {
+        if (onClickMarker == null) {
+            onClickMarker = mMap.addMarker(new MarkerOptions().position(point));
+        }
+        onClickMarker.setPosition(point);
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_LOCATION) {
+            if (!(grantResults.length == 1 && grantResults[0] == PERMISSION_GRANTED)) {
+                Toast.makeText(this, PERMISSION_NOT_GRANTED_TOAST, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        checkLocationPermission();
+        lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
     }
 }
--- GpsTracker/app/src/main/java/com/github/aint/gpstracker/service/LocationService.java
+++ GpsTracker/app/src/main/java/com/github/aint/gpstracker/service/LocationService.java
@@ -1,6 +1,5 @@
 package com.github.aint.gpstracker.service;
 
-import android.Manifest;
 import android.app.Service;
 import android.content.Intent;
 import android.location.Location;
@@ -9,48 +8,44 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v4.app.ActivityCompat;
-import android.util.Log;
 import android.widget.Toast;
 
 import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 import static com.github.aint.gpstracker.activity.MainActivity.NEW_LOCATION_ACTION;
 
-public class GpsService extends Service implements LocationListener {
+public class LocationService extends Service implements LocationListener {
+
+    public static final String LAT_ATTRIBUTE = "lat";
+    public static final String LNG_ATTRIBUTE = "lng";
 
     private static final String PERMISSION_NOT_GRANTED_TOAST = "Permission to location isn't granted";
 
-    public GpsService() {
-    }
-
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) != PERMISSION_GRANTED) {
+        if (checkLocationPermission()) {
             Toast.makeText(this, PERMISSION_NOT_GRANTED_TOAST, Toast.LENGTH_LONG).show();
             return 0;
         }
 
-//        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, this);
+        ((LocationManager) getSystemService(LOCATION_SERVICE))
+                .requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, this);
 
         return super.onStartCommand(intent, flags, startId);
     }
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        throw new UnsupportedOperationException("Not yet implemented");
+    private boolean checkLocationPermission() {
+        return ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) != PERMISSION_GRANTED;
     }
 
     @Override
     public void onLocationChanged(Location location) {
         Intent intent = new Intent(NEW_LOCATION_ACTION);
-        intent.putExtra("lat", location.getLatitude());
-        intent.putExtra("lng", location.getLongitude());
+        intent.putExtra(LAT_ATTRIBUTE, location.getLatitude());
+        intent.putExtra(LNG_ATTRIBUTE, location.getLongitude());
         sendBroadcast(intent);
-
     }
 
     @Override
@@ -66,6 +61,11 @@ public class GpsService extends Service implements LocationListener {
     @Override
     public void onProviderDisabled(String provider) {
 
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
 }
