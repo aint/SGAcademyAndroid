e34f089c89e365796595773139d7edcc97b74f23
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2016-08-18 14:15:51 +0300

    Init GpsTracker

---
+++ GpsTracker/app/src/main/java/com/github/aint/gpstracker/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.github.aint.gpstracker.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.aint.gpstracker.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
